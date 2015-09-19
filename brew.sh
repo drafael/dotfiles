7b9b9c3af59f1fe9b5f5ef936bbb02d7f9ca4124
Denys Rafael <denys.rafael@icloud.com>
2015-09-19 17:43:40 +0300

    Homebrew and iTerm2 fixes.

--- brew.sh
+++ brew.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
 
 # Install command-line tools
 brew tap Homebrew/bundle
